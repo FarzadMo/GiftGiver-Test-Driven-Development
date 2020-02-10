fe7d732eb763fb66775371f5d9dd3ecf6c63cd66
Fred M <farzad.moghbel@gmail.com>
2020-02-09 22:30:59 -0800

    add gift test completed

--- gift-giver/src/components/App.test.js
+++ gift-giver/src/components/App.test.js
@@ -4,8 +4,18 @@ import App from "./App";
 
 const app = shallow(<App />);
 
-//Unit test
+//Unit tests
 
 it("rendors correctly", () => {
   expect(app).toMatchSnapshot();
+});
+
+it("initializes the `state` with an empty array", () => {
+  expect(app.state().gifts).toEqual([]);
+});
+
+it("adds a new gift to `state` when clicking the `add gift` button", () => {
+  app.find(".btn-add").simulate("click");
+
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
