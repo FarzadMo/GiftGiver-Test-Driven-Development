9fd27a050293476f65c92f8df2be0d766bd80dd7
Fred M <farzad.moghbel@gmail.com>
2020-02-12 15:22:59 -0800

    the remove button tested successfully

--- gift-giver/src/components/Gift.js
+++ gift-giver/src/components/Gift.js
@@ -32,6 +32,13 @@ class Gift extends Component {
             />
           </FormGroup>
         </Form>
+        <Button
+          className="btn-remove"
+          onClick={() => this.props.removeGift(this.props.gift.id)}
+        >
+          {" "}
+          Remove Gift
+        </Button>
       </div>
     );
   }
